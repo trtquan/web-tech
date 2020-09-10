a73fe5c4afc483ef275252b7699e6bf755762ef7
quantran <trtquan@gmail.com>
2020-09-10 12:07:05 +0700

    scope

--- javascript/grammar-type/evaluating-variables.js
+++ javascript/grammar-type/evaluating-variables.js
@@ -13,7 +13,7 @@ const n = null;
 console.log(n + 2);
 
 
-// Variable scope(where can i access a variable)
+// Variable scope(where can i access a variable) 
 
 // Hoisting execution context is created in 2 phases
 /** first phase is called the creation phase in that phase we have the global object

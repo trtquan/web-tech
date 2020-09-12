bebc1f977fe40e611bb8305533c9d763254c7d0c
quantran <trtquan@gmail.com>
2020-09-12 12:14:35 +0700

    bind call

--- javascript/control-flow/expressions.js
+++ javascript/control-flow/expressions.js
@@ -21,7 +21,7 @@
       // Where a function uses the this keyword in its body,
       // its value can be bound to a particular object in 
       // the call using the call() or apply() methods which all functions inherit from
-        //  The bind method
+        //  The bind method 
           // Calling f.bind(someObject) creates a new function with the same body and scope as f,
           // but where this occurs in the original function, 
           // in the new function it is permanently bound to the first argument of bind
